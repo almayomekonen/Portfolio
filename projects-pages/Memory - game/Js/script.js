46019a67e1e995e65aa73a0f6193bd771b842872
alamayomekonen <alamayo274@gmail.com>
2023-11-29 16:12:51 -0800

    push snake

--- projects-pages/Memory - game/Js/script.js
+++ projects-pages/Memory - game/Js/script.js
@@ -12,13 +12,11 @@ for (let i = 1; i <= amount; i++) {
 }
 
 for (let i = 1; i <= amount * 2; i++) {
-  // אינדקס רנדומלי מהמערך
   const rand = Math.floor(Math.random() * numbers.length);
   const div = document.createElement("div");
   div.innerHTML = `<span>${numbers[rand]}</span>`;
   board.appendChild(div);
 
-  // בכדי שלא יהיו כפילויות, לאחר שהשתמשנו במיקום מסויים, הסרנו אותו מהמערך
   numbers.splice(rand, 1);
 
   div.addEventListener("click", (ev) => {
